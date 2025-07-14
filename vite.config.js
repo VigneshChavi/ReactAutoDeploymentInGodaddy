490144cf1f8d1dfaf09fdb60c5c8392f607eb8fd
VigneshChavi <75349813+VigneshChavi@users.noreply.github.com>
2025-07-14 17:59:23 +0530

    Auto Deploy Using Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // if you're using React
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Use '/' for custom domain
 })
