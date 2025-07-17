17e69ccc49f12769e5ff58641132d7e4b4aebb8c
VigneshChavi <75349813+VigneshChavi@users.noreply.github.com>
2025-07-17 15:51:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ function App() {
   return (
     <>
       <div>
-        {/* <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a> */}
+        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
         <p>
